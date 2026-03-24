94c76ecf9c17518d9871a1fe398e6c0e3ca38dde
Nguyễn Phương Chi <25020056@vnu.edu.vn>
2026-03-24 18:52:02 +0700

    origin

--- src/main/java/com/auction/client/MainApp.java
+++ src/main/java/com/auction/client/MainApp.java
@@ -1,15 +1,4 @@
 package com.auction.client;
 
 public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Hello anh em");
-        //hello pchi
-        // hmai chó
-        //chi hâm
-        //hello hưng
-        //fufu
-        //hello quỳnh
-        //sao ko dc
-        //jkdch
-    }
 }
