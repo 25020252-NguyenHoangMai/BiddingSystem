848888b0f739927bfdd4555bcf0070a5c115e0a4
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-03-24 10:21:59 +0700

    hello hung

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World and Hưng");
     }
 }
