3290a75ca54ecb1bb8f90f87543fbad35ccc72d0
Quynh <25020341@vnu.edu.vn>
2026-03-28 21:59:08 +0700

    hoàn thành OOP Model

--- src/main/java/com/auction/server/model/AuctionSession.java
+++ src/main/java/com/auction/server/model/AuctionSession.java
@@ -1,4 +1,65 @@
 package com.auction.server.model;
+import java.time.LocalDateTime;
 
-public class AuctionSession {
+public class AuctionSession extends Entity {
+    private Item item;
+    private double currentPrice;
+    private String currentWinnerId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String status; // "OPEN", "RUNNING", "FINISHED", "CANCELLED"
+
+    public AuctionSession() { super(); }
+
+    public AuctionSession(String id, Item item, LocalDateTime startTime, LocalDateTime endTime) {
+        super(id);
+        this.item = item;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.currentPrice = item.getStartingPrice();
+        this.status = "OPEN";
+        this.currentWinnerId = null;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getCurrentWinnerId() {
+        return currentWinnerId;
+    }
+    public void setCurrentWinnerId(String currentWinnerId) {
+        this.currentWinnerId = currentWinnerId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/auction/server/model/BidTransaction.java
+++ src/main/java/com/auction/server/model/BidTransaction.java
@@ -1,4 +1,47 @@
 package com.auction.server.model;
+import java.time.LocalDateTime;
 
-public class BidTransaction {
+public class BidTransaction extends Entity {
+    private String auctionId;
+    private String bidderId;
+    private double bidAmount;
+    private LocalDateTime bidTime;
+
+    public BidTransaction() { super(); }
+
+    public BidTransaction(String id, String auctionId, String bidderId, double bidAmount) {
+        super(id);
+        this.auctionId = auctionId;
+        this.bidderId = bidderId;
+        this.bidAmount = bidAmount;
+        this.bidTime = LocalDateTime.now();
+    }
+
+    public String getAuctionId() {
+        return auctionId;
+    }
+    public void setAuctionId(String auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public String getBidderId() {
+        return bidderId;
+    }
+    public void setBidderId(String bidderId) {
+        this.bidderId = bidderId;
+    }
+
+    public double getBidAmount() {
+        return bidAmount;
+    }
+    public void setBidAmount(double bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public LocalDateTime getBidTime() {
+        return bidTime;
+    }
+    public void setBidTime(LocalDateTime bidTime) {
+        this.bidTime = bidTime;
+    }
 }
