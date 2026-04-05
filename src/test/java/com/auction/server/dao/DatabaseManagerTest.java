b3d7e4da6f59dff2102cbfffc55ba137526ccce3
Quynh <25020341@vnu.edu.vn>
2026-04-06 01:33:21 +0700

    hoàn thiện kết nối database và unit test

---
+++ src/test/java/com/auction/server/dao/DatabaseManagerTest.java
@@ -0,0 +1,21 @@
+package com.auction.server.dao;
+
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseManagerTest {
+
+    @Test
+    void testConnection() {
+        System.out.println("Đang kiểm tra kết nối");
+        try (Connection conn = DatabaseManager.getConnection()) {
+            assertNotNull(conn, "Lỗi, Connection bị null!");
+            assertFalse(conn.isClosed(), "Kết nối đã bị đóng");
+            System.out.println("Kết nối SQL Server thành công");
+        } catch (SQLException e) {
+            fail("Kết nối thất bại! Lỗi: " + e.getMessage());
+        }
+    }
+}
