92579e5c7fe5ec9f7461b7360ccb88f4ae6970f2
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-04-15 02:37:13 +0700

    hoàn thiện phương thức addItem() - C/CRUD - ItemDAO

--- src/main/java/com/auction/server/dao/itemDAO.java
+++ src/main/java/com/auction/server/dao/itemDAO.java
@@ -1,4 +1,51 @@
 package com.auction.server.dao;
 
+import com.auction.model.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class itemDAO {
+    public boolean addItem(Item item, User user) {
+
+        String randomID = java.util.UUID.randomUUID().toString();
+        item.setId(randomID);
+
+        String sql = "INSERT INTO Item (id, name, description, itemType, sellerID, startingPrice, model, engineType, mileage, brand, artist) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, item.getId());
+            ps.setString(2, item.getName());
+            ps.setString(3, item.getDescription());
+            ps.setString(4, item.getItemType());
+            ps.setString(5, user.getId());
+            ps.setDouble(6, item.getStartingPrice());
+
+            if (item instanceof Vehicle vehicle) {
+                ps.setString(7, vehicle.getModel());
+                ps.setString(8, vehicle.getEngineType());
+                ps.setInt(9, vehicle.getMileage());
+                ps.setNull(10, java.sql.Types.NVARCHAR);
+                ps.setNull(11, java.sql.Types.NVARCHAR);
+            } else if (item instanceof Electronics electronics) {
+                ps.setNull(7, java.sql.Types.NVARCHAR);
+                ps.setNull(8, java.sql.Types.NVARCHAR);
+                ps.setNull(9, java.sql.Types.NVARCHAR);
+                ps.setString(10, electronics.getBrand());
+                ps.setNull(11, java.sql.Types.NVARCHAR);
+            } else if (item instanceof Art art) {
+                ps.setNull(7, java.sql.Types.NVARCHAR);
+                ps.setNull(8, java.sql.Types.NVARCHAR);
+                ps.setNull(9, java.sql.Types.NVARCHAR);
+                ps.setNull(10, java.sql.Types.NVARCHAR);
+                ps.setString(11, art.getArtist());
+            }
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
