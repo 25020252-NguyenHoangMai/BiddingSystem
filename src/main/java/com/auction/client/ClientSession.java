fb2a8ecf87f57503daa61b9775b9ecd97e27c54a
Nguyễn Phương Chi <25020056@vnu.edu.vn>
2026-04-19 00:50:14 +0700

    viết clientsession

--- src/main/java/com/auction/client/ClientSession.java
+++ src/main/java/com/auction/client/ClientSession.java
@@ -1,4 +1,15 @@
 package com.auction.client;
 
+import com.auction.model.User;
+
 public class ClientSession {
+    private static User currentUser;
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
 }
