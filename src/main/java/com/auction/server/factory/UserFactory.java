892c99e555b36dc31b0156866ad1809a5cd566f8
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-04-20 05:03:16 +0700

    sửa switch thành return switch

--- src/main/java/com/auction/server/factory/UserFactory.java
+++ src/main/java/com/auction/server/factory/UserFactory.java
@@ -10,15 +10,13 @@ import com.auction.server.dto.UserDTO;
 public class UserFactory {
 
     public static User createUser(UserDTO data) {
-        switch (data.getRole() == null ? "" : data.getRole().toUpperCase()) {
-            case "ADMIN":
-                return new Admin(data.getId(), data.getUsername(), data.getPassword(), data.getFullName());
-            case "BIDDER":
-                return new Bidder(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getBalance());
-            case "SELLER":
-                return new Seller(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getStoreName());
-            default:
-                throw new RuntimeException("Role không hợp lệ: " + data.getRole());
-        }
+        return switch (data.getRole() == null ? "" : data.getRole().toUpperCase()) {
+            case "ADMIN" -> new Admin(data.getId(), data.getUsername(), data.getPassword(), data.getFullName());
+            case "BIDDER" ->
+                    new Bidder(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getBalance());
+            case "SELLER" ->
+                    new Seller(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getStoreName());
+            default -> throw new RuntimeException("Role không hợp lệ: " + data.getRole());
+        };
     }
 }
