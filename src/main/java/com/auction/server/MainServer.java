85bda491b8ff6321925c79d363ba523d3dd14cc5
Nguyễn Phương Chi <25020056@vnu.edu.vn>
2026-04-20 17:28:26 +0700

    chuyển login và register từ userservice sang authcontroller bên server

--- src/main/java/com/auction/server/MainServer.java
+++ src/main/java/com/auction/server/MainServer.java
@@ -1,9 +1,25 @@
 package com.auction.server;
 
+import com.auction.server.controller.AuctionController;
+import com.auction.server.network.SocketServer;
+import com.auction.server.service.AntiSnipingService;
+import com.auction.server.service.AutoBiddingService;
+import com.auction.server.service.BiddingService;
+import com.auction.server.service.SessionService;
+import com.auction.server.service.UserService;
+
 import com.auction.server.network.SocketServer;
 
 public class MainServer {
     public static void main(String[] args) {
+        UserService userService = new UserService();
+        SessionService sessionService = new SessionService();
+        AutoBiddingService autoBiddingService = new AutoBiddingService();
+        AntiSnipingService antiSnipingService = new AntiSnipingService();
+        //BiddingService biddingService = new BiddingService(sessionService, autoBiddingService, antiSnipingService);
+
+        //AuctionController auctionController = new AuctionController(userService, sessionService, biddingService, autoBiddingService, antiSnipingService);
+
         SocketServer server = new SocketServer();
         server.startServer();
     }
--- src/main/java/com/auction/server/controller/AuctionController.java
+++ src/main/java/com/auction/server/controller/AuctionController.java
@@ -8,19 +8,19 @@ import com.auction.server.service.UserService;
 
 public class AuctionController {
 
-    private UserService userService;
+    private final AuthController authController;
 
     public AuctionController() {
-        this.userService = new UserService();
+        this.authController = new AuthController();
     }
 
     public Response handleRequest(Request request) {
         if (request instanceof LoginRequest loginRequest) {
-            return userService.login(loginRequest);
+            return authController.login(loginRequest);
         }
 
         if (request instanceof RegisterRequest registerRequest) {
-            return userService.register(registerRequest);
+            return authController.register(registerRequest);
         }
         return new Response(false, "Unknown request") {};
     }
