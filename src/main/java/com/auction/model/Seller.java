854cee8b8d2b48a890ec7fb5736f7e8ec42f950e
Quynh <25020341@vnu.edu.vn>
2026-04-20 22:07:39 +0700

    sửa seller

--- src/main/java/com/auction/model/Seller.java
+++ src/main/java/com/auction/model/Seller.java
@@ -4,21 +4,12 @@ import java.io.Serializable;
 
 public class Seller extends User implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String storeName;
     public Seller() {
         super();
     }
 
     public Seller(String id, String username, String password, String fullName, String storeName) {
         super(id, username, password, fullName, "SELLER");
-        this.storeName = storeName;
     }
 
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
 }
