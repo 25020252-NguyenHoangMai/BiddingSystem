e70666bd838cabc83e25c448e82022e6d523056c
Nguyễn Phương Chi <25020056@vnu.edu.vn>
2026-04-21 11:16:26 +0700

    đổi user sang userdto trong clientsession

--- src/main/java/com/auction/client/ClientSession.java
+++ src/main/java/com/auction/client/ClientSession.java
@@ -1,15 +1,23 @@
 package com.auction.client;
 
-import com.auction.model.User;
+import com.auction.server.dto.UserDTO;
 
 public class ClientSession {
-    private static User currentUser;
+    private static UserDTO currentUser;
 
-    public static User getCurrentUser() {
+    public static UserDTO getCurrentUser() {
         return currentUser;
     }
 
-    public static void setCurrentUser(User user) {
+    public static void setCurrentUser(UserDTO user) {
         currentUser = user;
+    }
+
+    public static void clear() {
+        currentUser = null;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
     }
 }
