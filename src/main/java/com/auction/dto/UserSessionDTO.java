446df557a372b3ba8435a6070703620121d18d67
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-04-21 12:06:01 +0700

    thêm UserSessionDTO sửa UserFromDTOFactory

---
+++ src/main/java/com/auction/dto/UserSessionDTO.java
@@ -0,0 +1,54 @@
+package com.auction.dto;
+
+public class UserSessionDTO {
+    private String id;
+    private String username;
+    private String fullName;
+    private String role;
+    private double balance;
+    private boolean sellerEnabled;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isSellerEnabled() {return sellerEnabled;}
+
+    public void setSellerEnabled(boolean sellerEnabled) {this.sellerEnabled = sellerEnabled;}
+}
--- src/main/java/com/auction/server/factory/UserFromDTOFactory.java
+++ src/main/java/com/auction/server/factory/UserFromDTOFactory.java
@@ -3,7 +3,7 @@ package com.auction.server.factory;
 import com.auction.model.Admin;
 import com.auction.model.Bidder;
 import com.auction.model.User;
-import com.auction.server.dto.UserDTO;
+import com.auction.dto.UserDTO;
 
 
 public class UserFromDTOFactory {
@@ -12,9 +12,7 @@ public class UserFromDTOFactory {
         return switch (data.getRole() == null ? "" : data.getRole().toUpperCase()) {
             case "ADMIN" -> new Admin(data.getId(), data.getUsername(), data.getPassword(), data.getFullName());
             case "BIDDER" ->
-                    new Bidder(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getBalance());
-            case "SELLER" ->
-                    new Seller(data.getId(), data.getUsername(), data.getPassword(), data.getFullName());
+                    new Bidder(data.getId(), data.getUsername(), data.getPassword(), data.getFullName(), data.getRole(), data.getBalance());
             default -> throw new RuntimeException("Role không hợp lệ: " + data.getRole());
         };
     }
