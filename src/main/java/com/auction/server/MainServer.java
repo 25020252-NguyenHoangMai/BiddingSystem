45a57d8d12ce30ad93da55c7ff1d8b53f9133866
Nguyễn Phương Chi <25020056@vnu.edu.vn>
2026-04-26 17:25:34 +0700

    thêm itemservice và itemcontroller trong mainserver

--- src/main/java/com/auction/server/MainServer.java
+++ src/main/java/com/auction/server/MainServer.java
@@ -2,21 +2,27 @@ package com.auction.server;
 
 import com.auction.server.controller.AuctionController;
 import com.auction.server.controller.AuthController;
+import com.auction.server.controller.ItemController;
 import com.auction.server.network.SocketServer;
 import com.auction.server.service.AntiSnipingService;
 import com.auction.server.service.AutoBiddingService;
+import com.auction.server.service.ItemService;
 import com.auction.server.service.UserService;
 
 public class MainServer {
     public static void main(String[] args) {
         UserService userService = new UserService();
+        ItemService itemService = new ItemService();
+
         AuthController authController = new AuthController(userService);
+        ItemController itemController = new ItemController(itemService);
+
         //SessionService sessionService = new SessionService();
         AutoBiddingService autoBiddingService = new AutoBiddingService();
         AntiSnipingService antiSnipingService = new AntiSnipingService();
         //BiddingService biddingService = new BiddingService(sessionService, autoBiddingService, antiSnipingService);
 
-        AuctionController auctionController = new AuctionController(authController);
+        AuctionController auctionController = new AuctionController(authController,  itemController);
 
         SocketServer server = new SocketServer(5000, auctionController);
         server.startServer();
