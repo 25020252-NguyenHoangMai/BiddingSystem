cceecc2d02b498ad46d5f4215e7c2bb127246c67
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-04-29 17:43:23 +0700

    sử dụng connection pool

--- src/main/java/com/auction/server/dao/DatabaseManager.java
+++ src/main/java/com/auction/server/dao/DatabaseManager.java
@@ -1,29 +1,28 @@
 package com.auction.server.dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
     private static volatile DatabaseManager instance;
-
-    private static final String SERVER_NAME = "localhost\\SQLEXPRESS";
-    private static final String DB_NAME = "BiddingSystem";
-    private static final String USER = "sa";
-    private static final String PASS = "88888888";
-
-    private static final String CONNECTION_URL =
-            "jdbc:sqlserver://" + SERVER_NAME + ":1433;" +
-                    "databaseName=" + DB_NAME + ";" +
-                    "encrypt=true;trustServerCertificate=true;";
+    private DataSource dataSource;
 
     private DatabaseManager() {
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(System.getenv("DB_URL"));
+        config.setUsername(System.getenv("DB_USER"));
+        config.setPassword(System.getenv("DB_PASS"));
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+
+        dataSource = new HikariDataSource(config);
     }
+
     public static DatabaseManager getInstance() {
         if (instance == null) {
             synchronized (DatabaseManager.class) {
@@ -35,6 +34,6 @@ public class DatabaseManager {
         return instance;
     }
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(CONNECTION_URL, USER, PASS);
+        return dataSource.getConnection();
     }
 }
