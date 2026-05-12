1a782337f82e5101e9e9fe04dfb219e35bc0168f
Quynh <25020341@vnu.edu.vn>
2026-05-12 09:25:08 +0700

    gom nhóm bằng Nested cho userservice test + rút gọn tên + note

--- src/test/java/com/auction/server/service/UserServiceTest.java
+++ src/test/java/com/auction/server/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.auction.server.service;
 
+import org.junit.jupiter.api.Nested;
 import org.mindrot.jbcrypt.BCrypt;
 import com.auction.exception.AuctionException;
 import com.auction.exception.AuthenticationException;
@@ -23,18 +24,18 @@ import static org.mockito.Mockito.*;
 public class UserServiceTest {
 
     @Mock
-    private UserDAO userDAO; // Giả lập tầng Database
+    private UserDAO userDAO;
 
-    private UserService userService; // Nhét cái DAO giả vào UserService thật
+    private UserService userService;
 
-    // 1 cái để đăng ký, 1 cái là giả lập từ DB trả về
+    //1 cái để đăng ký, 1 cái là giả lập từ DB trả về
     private User testUser;
     private User testUserInDB;
 
     @BeforeEach
     void setUp() {
         userService = new UserService(userDAO);
-        // 1. Chuẩn bị User mới tinh (Mật khẩu chưa băm - dùng để test Register)
+        //user ms-mk chx băm
         testUser = new Bidder();
         testUser.setId("U01");
         testUser.setUsername("quynh_admin");
@@ -42,7 +43,7 @@ public class UserServiceTest {
         testUser.setFullName("Quỳnh");
         testUser.setRole("BIDDER");
 
-        // 2. Chuẩn bị User đã nằm trong Database (Mật khẩu ĐÃ BĂM - dùng để test Login)
+        //cbi user cũ mk đã băm
         testUserInDB = new Bidder();
         testUserInDB.setId("U01");
         testUserInDB.setUsername("quynh_admin");
@@ -56,71 +57,73 @@ public class UserServiceTest {
     //TEST REGISTER
 
 
-    @Test
-    void testRegister_UsernameDaTonTai_PhaiNemLoi() {
-        // TH1: tên đã có người dùng
-        when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
+    @Nested
+    class TestRegister {
+        @Test
+        void UsernameDaTonTai_PhaiNemLoi() {
 
-        assertThrows(AuctionException.class, () -> {
+            when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
+
+            assertThrows(AuctionException.class, () -> {
+                userService.register(testUser);
+            }, "Lỗi: Đăng ký trùng username mà không ném lỗi AuctionException!");
+
+            // Đảm bảo hàm insertUser không bao giờ được gọi
+            verify(userDAO, never()).insertUser(any());
+        }
+
+        @Test
+        void ThanhCong() {
+
+            when(userDAO.getUserByUsername("quynh_admin")).thenReturn(null);
+
             userService.register(testUser);
-        }, "Lỗi: Đăng ký trùng username mà không ném lỗi AuctionException!");
 
-        // Đảm bảo hàm insertUser không bao giờ được gọi
-        verify(userDAO, never()).insertUser(any());
-    }
+            assertNotNull(testUser.getId(), "Lỗi: ID chưa được sinh tự động!");
 
-    @Test
-    void testRegister_ThanhCong() {
-        // TH2: Tên chưa ai dùng trả về null
-        when(userDAO.getUserByUsername("quynh_admin")).thenReturn(null);
+            assertEquals(60, testUser.getPassword().length(), "Lỗi: Mật khẩu chưa được băm!");
 
-        userService.register(testUser);
-
-        // Kiểm tra xem ID đã được tự động sinh ra chưa
-        assertNotNull(testUser.getId(), "Lỗi: ID chưa được sinh tự động!");
-        // Kiểm tra xem mật khẩu đã bị băm chưa (Độ dài chuỗi băm BCrypt thường là 60 ký tự)
-        assertEquals(60, testUser.getPassword().length(), "Lỗi: Mật khẩu chưa được băm!");
-
-        // Đảm bảo hàm insertUser đã được gọi đúng 1 lần để lưu xuống DB
-        verify(userDAO, times(1)).insertUser(testUser);
+            // Đảm bảo hàm insertUser đã được gọi đúng 1 lần để lưu xuống DB
+            verify(userDAO, times(1)).insertUser(testUser);
+        }
     }
 
 
+    //TEST LOGIN
 
-    // 2. TEST LOGIN
 
+    @Nested
+    class testLogin {
+        @Test
+        void UsernameKhongTonTai_PhaiNemLoi() {
+            when(userDAO.getUserByUsername("user_ma")).thenReturn(null);
 
-    @Test
-    void testLogin_UsernameKhongTonTai_PhaiNemLoi() {
-        when(userDAO.getUserByUsername("user_ma")).thenReturn(null);
+            assertThrows(AuthenticationException.class, () -> {
+                userService.login("user_ma", "123456");
+            });
+        }
 
-        assertThrows(AuthenticationException.class, () -> {
-            userService.login("user_ma", "123456");
-        });
+        @Test
+        void SaiMatKhau_PhaiNemLoi() {
+            //trả về mk dungds (password123) đã băm
+            when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
+
+            assertThrows(AuthenticationException.class, () -> {
+                userService.login("quynh_admin", "sai_mat_khau");//truyền sai mk
+            });
+        }
+
+        @Test
+        void ThanhCong() {
+            when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
+
+            User loggedInUser = userService.login("quynh_admin", "password123");
+
+            assertNotNull(loggedInUser, "Lỗi: Đăng nhập thành công phải trả về Object User");
+            assertEquals("quynh_admin", loggedInUser.getUsername());
+            assertNull(loggedInUser.getPassword(), "Lỗi: Mật khẩu không được xóa để bảo mật trước khi trả về!");
+        }
     }
-
-    @Test
-    void testLogin_SaiMatKhau_PhaiNemLoi() {
-        // Trả về User từ DB có mật khẩu đúng là "password123" (đã băm)
-        when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
-
-        assertThrows(AuthenticationException.class, () -> {
-            // Cố tình truyền mật khẩu sai
-            userService.login("quynh_admin", "sai_mat_khau");
-        });
-    }
-
-    @Test
-    void testLogin_ThanhCong() {
-        when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
-
-        User loggedInUser = userService.login("quynh_admin", "password123");
-
-        assertNotNull(loggedInUser, "Lỗi: Đăng nhập thành công phải trả về Object User");
-        assertEquals("quynh_admin", loggedInUser.getUsername());
-        assertNull(loggedInUser.getPassword(), "Lỗi: Mật khẩu không được xóa để bảo mật trước khi trả về!");
-    }
-
 
 
     //TEST CHANGE PASSWORD
@@ -140,35 +143,36 @@ public class UserServiceTest {
     //TEST UPDATE PROFILE
 
 
-    @Test
-    void testUpdateProfile_UsernameDaTonTaiCuaNguoiKhac_PhaiNemLoi() {
-        // Mình là U01, nhưng đổi username thành "mai_dao"
-        // mà "mai_dao" lại là của người khác (ID là U02)
-        User otherPerson = new Bidder(); // Đổi từ DTO sang Bidder
-        otherPerson.setId("U02"); // ID khác mình
-        otherPerson.setUsername("mai_dao");
+    @Nested
+    class testUpdateProfile {
+        @Test
+        void UsernameDaTonTaiCuaNguoiKhac_PhaiNemLoi() {
 
-        testUser.setUsername("mai_dao"); // thử đổi tên
-        when(userDAO.getUserByUsername("mai_dao")).thenReturn(otherPerson);
+            User otherPerson = new Bidder(); // Đổi từ DTO sang Bidder
+            otherPerson.setId("U02");
+            otherPerson.setUsername("mai_dao");
 
-        assertThrows(AuctionException.class, () -> {
+            testUser.setUsername("mai_dao");
+            when(userDAO.getUserByUsername("mai_dao")).thenReturn(otherPerson);
+
+            assertThrows(AuctionException.class, () -> {
+                userService.updateProfile(testUser);
+            });
+
+            verify(userDAO, never()).updateUser(any());
+        }
+
+        @Test
+        void ThanhCong() {
+
+            when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
+
             userService.updateProfile(testUser);
-        });
 
-        verify(userDAO, never()).updateUser(any());
+            // Đảm bảo lệnh update được chạy
+            verify(userDAO, times(1)).updateUser(testUser);
+        }
     }
-
-    @Test
-    void testUpdateProfile_ThanhCong() {
-        // đổi thành tên chưa ai dùng hoặc tên cũ
-        when(userDAO.getUserByUsername("quynh_admin")).thenReturn(testUserInDB);
-
-        userService.updateProfile(testUser);
-
-        // Đảm bảo lệnh update được chạy
-        verify(userDAO, times(1)).updateUser(testUser);
-    }
-
 
 
     //TEST GET USER BY ID & USERNAME
