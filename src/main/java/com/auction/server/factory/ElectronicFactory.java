1129e2b86720c2ecbd4bf627ff91a7d2f9afd84e
Quynh <25020341@vnu.edu.vn>
2026-05-14 01:26:20 +0700

    electronic factory

--- src/main/java/com/auction/server/factory/ElectronicFactory.java
+++ src/main/java/com/auction/server/factory/ElectronicFactory.java
@@ -1,4 +1,20 @@
 package com.auction.server.factory;
 
-public class ElectronicFactory {
+import com.auction.dto.ItemDTO;
+import com.auction.model.Electronics;
+import com.auction.model.Item;
+
+public class ElectronicFactory implements ItemFactory {
+    @Override
+    public Item createItem(ItemDTO dto) {
+        return new Electronics(
+                dto.getId(),
+                dto.getName(),
+                dto.getDescription(),
+                dto.getSellerId(),
+                dto.getStartingPrice(),
+                dto.getBrand()
+        );
+    }
+
 }
