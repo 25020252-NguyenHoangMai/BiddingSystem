1513474cd3c6aa5ebbfc0576316304141dc4a4a1
Nguyễn Hoàng Mai <25020252@vnu.edu.vn>
2026-05-27 14:53:47 +0700

    thêm class mồi để bật main app

---
+++ src/main/java/com/auction/client/ClientLauncher.java
@@ -0,0 +1,8 @@
+package com.auction.client;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        // Gọi trực tiếp hàm main của MainApp từ đây
+        MainApp.main(args);
+    }
+}
